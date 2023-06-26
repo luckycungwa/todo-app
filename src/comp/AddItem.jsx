ab03bf200a5e0caacd801ef43b0bc322f4b64746
luckycungwa <cungwalucky@gmail.com>
2023-06-26 15:57:15 +0200

    added new function for complete task

--- src/comp/AddItem.jsx
+++ src/comp/AddItem.jsx
@@ -6,8 +6,9 @@ function AddItem() {
   const [todos, setTodos] = useState([]);
 
   const handleInputChange = (e) => {
+    //Monitor the user's input & alert if the iput was recorded
     setNewTask(e.target.value);
-    console.log("Task Added!"); // Your comment
+    console.log("Task Added!"); 
   };
 
   const handleSubmit = (e) => {
@@ -17,7 +18,7 @@ function AddItem() {
 
   const AddTask = () => {
     if (!newTask) {
-      alert("You must add at least 1 task!");
+      alert("You must add at least 1 task!");       // Alert the user if they did not filll in the task
       return;
     }
 
@@ -32,14 +33,9 @@ function AddItem() {
 
   const deleteTask = (id) => {
     setTodos((prevList) => {
-      // check existing array before deleting item from array
-      prevList.filter((task) => task.id !== id);
-      console.log("SUCCESSFUL DELETE!");
+      return prevList.filter((task) => task.id !== id);
     });
-  };
-
-  const handleDelete = () => {
-    deleteTask(todos.id);
+    console.log("SUCCESSFUL DELETE!");
   };
 
   return (
@@ -64,7 +60,7 @@ function AddItem() {
                     <label className="label">Create New Task</label>
                     <input
                       className="input-field"
-                      placeholder="create new task..."
+                      placeholder="Add new task..."
                       type="text"
                       name="todoInput"
                       value={newTask}
@@ -76,19 +72,12 @@ function AddItem() {
                 <div className="form-field">
                   <div className="">
                     {/* TABLE FOR WORKER INFORMATION DISPLAY */}
-                    {todos.map((task) => {
-                      return (
-                        <div key={task.id}>
-                          <Items value={task.value} />
-                          <div className="delete">
-                            <button
-                              className="unbutton"
-                              onClick={() => deleteTask(task.id)}
-                            ></button>
-                          </div>
-                        </div>
-                      );
-                    })}
+                    {todos.map((task) => (
+                      <div key={task.id}>
+                      {/* set the values as per input and enable deletion from our component (Items)  */}
+                        <Items todo={task} onDelete={deleteTask} />
+                      </div>
+                    ))}
                   </div>
 
                   <br />
--- src/comp/Items.jsx
+++ src/comp/Items.jsx
@@ -1,24 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
-function Items(todos) {
-  
+function Items({ todo, onDelete }) {
+  //Creating states for when the task is complete
+  const [taskDone, setTaskDone] = useState(false);
 
-
+  const handleComplete = () => {
+    // use states if statement
+    setTaskDone(!taskDone);
+  };
   return (
-    // components as an  ITEM
     <>
       {/* THE ITEM COMPONENTS HERE */}
-
       {/* This is where each item (Index) from the array will be displayed */}
-
       <div className="">
         <div className="todo-item">
           <div className="category" />
           {/* Display the value recorded on the user input */}
-          <p>{todos.value}</p>
+          <p>{todo.value}</p>
           {/* components for functionality (DELETE | EDIT | MARK AS COMPLETE) */}
-          <div className="complete"></div>
-          
+          <div className="">
+            <button className="" onClick={handleComplete}>
+              {/* When the user clicks the button it should alternate between "complete" vs "incomplete" */}
+              {/* id statement to check current state */}
+              {taskDone ? <img src=""></img> : "Pendig"}
+            </button>
+          </div>
+          <div className=""></div>
+          {/* Delete corresponding task */}
+          <button
+            className="delete unbutton"
+            onClick={() => onDelete(todo.id)}
+          ></button>
         </div>
         <div></div>
       </div>
